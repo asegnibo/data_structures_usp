eed36e133bd685464964be43b7c1bd9f64df3c1e
Ashton <ashton.segnibo@hotmail.com>
2025-05-23 14:22:41 -0300

    teste de commit

--- Análise de Algoritimos/complexidade.2.py
+++ Análise de Algoritimos/complexidade.2.py
@@ -4,7 +4,7 @@ for i in range(1,n): # O(n)
     j = i
     while j > 1 : # O número de vezes que um número i pode ser dividido por 2 antes de chegar a 1 é aproximadamente log₂(i). O(log n)
         print(j)
-        j = j / 2
+        j = j / 2 
         
 # O(n) + O(log n) = O(n log n)
 # O(n log n) é a complexidade de tempo total do algoritmo.
