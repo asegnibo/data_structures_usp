6e2fff4f781d6e741a6c7cb3c0c7a6673048f8c6
Ashton <ashton.segnibo@hotmail.com>
2025-06-01 08:09:59 -0300

    tree

--- trees/binary_tree/activity.py
+++ trees/binary_tree/activity.py
@@ -6,25 +6,52 @@ class Node:
     
     def __str__(self):
         return str(self.data)
-    
-class BinaryTree:
-    def __init__(self, data=None):
-        if data:
-            node = Node(data)
-            self.root = node
+
+def construir_percorrer_arvore():
+    n = int(input())
+    nos_arvore = {}
+    raiz = None
+
+    for _ in range(n):
+        linha = input().split()
+        nome_no_atual = linha[0]
+
+        if nome_no_atual not in nos_arvore:
+            nos_arvore[nome_no_atual] = Node(nome_no_atual)
+            no_atual = nos_arvore[nome_no_atual]
+
+        if len(linha) > 1:
+            nome_pai = linha[1]
+            posicao = linha[2]
+
+            if nome_pai not in nos_arvore:
+                nos_arvore[nome_pai] = Node(nome_pai)
+            
+            no_pai = nos_arvore[nome_pai]
+
+            if posicao == 'e':
+                no_pai.left = no_atual
+            elif posicao == 'd':
+                no_pai.right = no_atual
+        
         else:
-            self.root = None
+            raiz = no_atual
+
+    resultado_pre_ordem=[]
+
+    def pre_ordem_travessia(no):
+            if no:
+                resultado_pre_ordem.append(no.data) # Visita a raiz
+                pre_ordem_travessia(no.left)   # Percorre a subárvore esquerda
+                pre_ordem_travessia(no.right)    # Percorre a subárvore direita
+
+    pre_ordem_travessia(raiz)
     
-    def simetric_traversal(self, node = None):
-        if node is None:
-            node = self.root
-        if node.left:
-            print('(', end="")
-            self.simetric_traversal(node.left)
-        print(node, end = '')
-        if node.right:
-            self.simetric_traversal(node.right)
-            print(')', end="")
-    
-n = int(input())
-root = BinaryTree(input())
+    # Imprime o resultado formatado
+    print(';'.join(resultado_pre_ordem))
+
+# Chama a função principal para executar o programa
+construir_percorrer_arvore()
+
+
+
